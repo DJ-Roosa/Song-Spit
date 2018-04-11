d7d9d8287ae8ce2b1529725377697ff32896108f
Rob Martin <rob.martin.5758@gmail.com>
2018-04-11 19:38:39 -0400

    add spotify to html

--- assets/js/spotifyinitialize.js
+++ assets/js/spotifyinitialize.js
@@ -6,4 +6,4 @@
 // the access token is added to the redirect link
 // the app then parses out the access_token and uses it in the Header on API calls
 console.log("spotifyinitialize");
-window.location.href = "https://accounts.spotify.com/authorize?client_id=e3f88cfb7e5c4c218a192eccbbad9249&response_type=token&redirect_uri=https://dj-roosa.github.io/Song-Spit/spotifytest.html&scope=user-read-private";
+window.location.href = "https://accounts.spotify.com/authorize?client_id=e3f88cfb7e5c4c218a192eccbbad9249&response_type=token&redirect_uri=https://dj-roosa.github.io/Song-Spit/index.html&scope=user-read-private";
