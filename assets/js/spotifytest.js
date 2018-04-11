2ca90d5906ab67571ffa49d39f8365083767adb2
Rob Martin <Rob.martin.5758@gmail.com>
2018-04-11 13:42:11 -0400

    Update spotifytest.js

--- assets/js/spotifytest.js
+++ assets/js/spotifytest.js
@@ -1,20 +1,44 @@
 $("#searchSongBtn").on("click", function (event) {
     event.preventDefault();
+    // let searchSong = $("#searchSong").val().trim();
+    // let searchArtist = $("#searchArtist").val().trim();
+    // let searchAlbum = $("#searchAlbum").val().trim();
+    // let songLink = spotifySearch(searchSong, searchArtist, searchAlbum);
+
+    // console.log(songLink.link);
+    // //returnValues = { link: linkReturn }
+    // if (songLink.link === "#") {
+    //     console.log("No Match Found");
+    //     $("#link").val = "No Match Found";
+    // } else {
+    //     let {
+    //         link
+    //     } = songLink.link;
+    //     $("#link").val = "<a href src=" + link + "/>";
+    //     console.log("Link is: " + link);
+    // };
+
+// Create a function that accepts another function as an argument
+const callbackAcceptingFunction = (fn) => {
     let searchSong = $("#searchSong").val().trim();
     let searchArtist = $("#searchArtist").val().trim();
     let searchAlbum = $("#searchAlbum").val().trim();
     let songLink = spotifySearch(searchSong, searchArtist, searchAlbum);
 
-    console.log(songLink.link);
-    //returnValues = { link: linkReturn }
-    if (songLink.link === "#") {
-        console.log("No Match Found");
-        $("#link").val = "No Match Found";
-    } else {
-        let {
-            link
-        } = songLink.link;
-        $("#link").val = "<a href src=" + link + "/>";
-        console.log("Link is: " + link);
-    };
+    return fn(songLink)
+  }
+  
+  // Callback gets arguments from the above call
+  const callback = (link) => {
+    return link
+  }
+  
+  // Passing a callback into a callback accepting function
+  const result = callbackAcceptingFunction(callback)
+  console.log(result) // 6
+
+
+
+
+
 });
